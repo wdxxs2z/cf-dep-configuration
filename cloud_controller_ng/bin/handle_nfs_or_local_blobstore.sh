e55ff933933d42370dfe35d0fa7f274a444f75de
wdxxs2z <jackyuan88726@gmail.com>
2014-07-07 13:32:09 +0800

    init script

---
+++ cloud_controller_ng/bin/handle_nfs_or_local_blobstore.sh
@@ -0,0 +1,19 @@
+
+
+echo "Preparing local droplet directory"
+
+mkdir -p /var/vcap/nfs/shared
+chown vcap:vcap /var/vcap/nfs/shared
+
+
+
+mkdir -p /var/vcap/nfs/shared
+chown vcap:vcap /var/vcap/nfs/shared
+
+
+
+mkdir -p /var/vcap/nfs/shared
+chown vcap:vcap /var/vcap/nfs/shared
+
+
+
